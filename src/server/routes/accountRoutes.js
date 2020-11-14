8dd565c340a3f71da51f89a1dfd7aaec769e1743
shaj684 <48861103+shaj684@users.noreply.github.com>
2020-11-14 15:19:47 -0500

    finishing preliminary model

--- src/server/routes/accountRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const accountController = require('../controllers/accountController');
-
-router.get('/:id', accountController.account_details);
-router.delete('/:id', accountController.account_delete);
-
-module.exports = router;
