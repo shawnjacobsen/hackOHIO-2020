f2ea7a66c9c08d89e49d0b14851fc5ae3be7b29d
GavinZ <gzwayer@gmail.com>
2020-11-14 16:29:11 -0500

    hello

--- src/client/src/Dashboard.js
+++ src/client/src/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./NavBar/Navbar";
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './happypeople.jpeg'
 
--- src/client/src/NavBar/Form.js
+++ src/client/src/NavBar/Form.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import {useForm} from "react-hook-form";
 import { useState, useEffect } from 'react';
 
+
+
+
+
+
 function FormsAndInputs(){ 
     const { register, handleSubmit } = useForm(); 
     const onSubmit = (values) => console.log(values);
@@ -15,83 +20,96 @@ const [kids,setkids] = useState(0);
 const [rev,setrev] = useState(0); 
 const [exp,setexp] = useState(0);   
 const [loc,setloc] = useState("homeless"); 
-const [stonk,setstonk] = useState(0);  
+const [stonk,setstonk] = useState(0); 
+const [finA,setfinA] = useState(0);  
 
-useEffect (()=> { 
-   document.title = `Sup ${rev}`;
-        });
+const [finalI, setfinalI] = useState(0);
+const [finalA,setfinalA] = useState(0);
+const [stockf,setstockf] = useState(0); 
+const [fkids,setfkids] = useState(0); 
+const [costL,setcostL] = useState(0);
 
     return( 
         <div className="FormsAndInputs"> 
          <form onSubmit={handleSubmit(onSubmit)}> 
           
           
-                <h1>What are you most interested in doing on Stonks.io?</h1>
-                <label for="capGrowth"> 
+                <h1 className="className">Possible Goals for Stonks.io?</h1>
+                <label for="capGrowth" className="capGrow"> 
                         <input  name="capGrowth" id="capGrowth" type="checkbox" ref={
                              register()
                              } onInput = {()=>setcapG(capG ==1 ? 0:1)}/>  
-                       Capital Growth </label>
+                       <space></space> Capital Growth </label>
                     <div></div> 
                    
-                    <label for="startBus">
+                    <label for="startBus" className="startbiz">
                      <input name="startBus"  type="checkbox" id="startBus" ref={
                   
                   register()
                      } onInput = {()=>setbus(bus ==1 ? 0:1)} />
-                    Starting a Business</label>
+                    <space></space> Starting a Business</label>
                     <div></div>
-                    <label for="finCol">
+                    <label for="finCol" className="finny">
                     <input name="finCol" type="checkbox" id="finCol" ref={
                 register()
                  } onInput = {()=>setfinC(finC ==1 ? 0:1)}/>
-                Financing College</label>
+                <space></space> Financing College</label>
              <div></div>
-             <label for="livingExpense">
+             <label for="livingExpense" className="livE">
                 <input name="livingExpense" type="checkbox" id="livingExpense" ref={
                 register()
                 } onInput = {()=>setlE(lE ==1 ? 0:1)}/>
-                Budget Living Expenses</label>
+               <space></space> Budget Living Expenses</label>
                     <div></div>
           
           
-                <h1>Information</h1>
+                <h1 className="info">Personal Information</h1>
           
       
-                <label> 
-                    Income           
+                <label className="inc"> 
+                    Income <space></space>          
                     <input type="number" name="income" ref={register} onChange={event=>setrev(event.target.value)}/>
                     </label> 
-                <label> 
-                    Expenses 
+                <label className="expe"> 
+                    Expenses <space></space>
                     <input type="number" name="expense" ref={register} onChange={event=>setexp(event.target.value)}/>
                 </label> 
-                <label> 
-                    Location 
+                <label className="fib"> 
+                    State Fib Code <space></space>
                     <input type="text" name="location" ref={register} onChange={event=>setloc(event.target.value)}/>
                 </label>
-                <label> 
+                <label className="child"> 
                     Childern 
                     <input type="number" name="children" ref={register} onChange={event=>setkids(event.target.value)}/>
                 </label>
-                <label>Do you have a Bank?</label>
-                <label>Yes
+                <label className="bannk">Do you have a Bank?</label>
+                <label className="bannky"><space></space> Yes
                 <input name="Bank" type="radio" value="Yes" ref={register({required: true })} onInput = {()=>setbank(bank ==1 ? 0:1)} /> 
-                </label><label>No
+                </label><label className="bannkn"><space></space> No
                 <input name="Bank" type="radio" value="No" ref={register({required: true })} onInput = {()=>setbank(bank ==1 ? 0:1)} />
                 </label>
                 
 
-                <label>Do you have a Mutual Funds or Stocks?</label>
-                    <label>Yes
+                <label className="stonkk">Do you have a Mutual Funds or Stocks?</label>
+                    <label className="stonkky"><space></space> Yes 
                     <input name="mutualfund" type="radio" value="Yes" ref={register({required: true })} onInput = {()=>setstonk(stonk ==1 ? 0:1)} /> 
-                    </label><label>No
+                    </label><label className="stonkkn"> No 
                     <input name="mutualfund" type="radio" value="No" ref={register({required: true })} onInput = {()=>setstonk(stonk ==1 ? 0:1)}/>
                     </label> 
 
+                    <label className="finAS">Do you have Finacial Assistance?</label>
+                    <label className="finASy"><space></space> Yes
+                    <input name="finass" type="radio" value="Yes" ref={register({required: true })} onInput = {()=>setfinA(finA ==1 ? 0:1)} /> 
+                    </label ><label className="finASn"><space></space> No
+                    <input name="finass" type="radio" value="No" ref={register({required: true })} onInput = {()=>setfinA(finA ==1 ? 0:1)}/>
+                    </label> 
+
+
 
                  <label>
-             <button type="submit" >Submit</button>
+             <button type="submit" onClick ={()=>setfinalI(rev)} onClick ={()=>setfinalA(finA)} onClick ={()=>setstockf(stonk)}
+             onClick ={()=>setfkids(kids)} onClick ={()=>setcostL(loc)}
+             >Submit</button>
              </label>
 
             </form>
