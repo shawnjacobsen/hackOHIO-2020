6b1722d549013c941de72b761f326348121e21df
GavinZ <gzwayer@gmail.com>
2020-11-14 03:42:07 -0500

    hello

--- src/client/src/NavBar/Form.js
+++ src/client/src/NavBar/Form.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react' 
+import {useForm} from "react-hook-form";
+
+function FormsAndInputs(){ 
+    const { register, handleSubmit } = useForm(); 
+    const onSubmit = (values) => console.log(values);
+    return( 
+        <div className="FormsAndInputs"> 
+         <form onSubmit={handleSubmit(onSubmit)}> 
+          <label> 
+              Email           
+              <input type="email" name="email" ref={register}/>
+              </label> 
+          <label> 
+              Password 
+              <input type="password" name="password" ref={register}/>
+          </label> 
+          <label> 
+              Location Name 
+              <input type="text" name="location" ref={register}/>
+          </label>
+          <label>Do you have a Bank?</label>
+        <label>Yes
+        <input name="Bank" type="radio" value="Yes" ref={register({required: true })}/> 
+        </label><label>No
+        <input name="Bank" type="radio" value="No" ref={register({required: true })}/>
+        </label>
+            <label>
+            <button type="submit">Submit</button>
+            </label>
+
+         </form>
+        </div>
 
 
-class FormsAndInputs extends Component{ 
+    )
 
-    handleSubmit = () => { 
 
-    } 
-    handleInputChange = () => { 
 
-    } 
-    render () { 
-        return ( 
-            <div> 
-                <h1>Goals</h1> 
-                <form> 
-                    <p><input type='text' placeholder='Your Name' name='name' /></p> 
-                    <p><button>Submit</button></p>
-                </form>
-            </div>
-        )
-    }
-} 
- export default FormsAndInputs;
-
+}
+export default FormsAndInputs;
