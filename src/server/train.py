9d584cd63f89b898c281240f8c87c8a4b64b199a
shaj684 <48861103+shaj684@users.noreply.github.com>
2020-11-14 22:22:41 -0500

    appending to app.py

--- src/server/train.py
+++ src/server/train.py
@@ -34,7 +34,7 @@ y = df_array[:,7:12]
 #y = df[['relocate', 'get_bank_account', 'get_credit_card', 'fin_diversification', 'fin_consolidation']]
 
 # Split Data 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0001, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=99)
 print(X_train.shape)
 
 ### Problem Transform
